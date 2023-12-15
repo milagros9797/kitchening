539da154c1b5318a225ef7e0d68010f918429c0b
Milagros <miilyailen97@gmail.com>
2023-12-15 00:47:27 -0300

    agrego vista de la informacion del producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,9 +1,19 @@
+const {leerJSON} = require('../data')
 module.exports = {
     add : (req,res) => {
         return res.render('products/product-add')
     },
     detail : (req,res) => {
-        return res.render('products/product-detail')
+
+        const {id} = req.params;
+
+        const products = leerJSON('products');
+
+        const product = products.find(product => product.id === +id);
+
+        return res.render('products/product-detail', {
+            ...product
+        })
     },
     edit : (req,res) => {
         return res.render('products/product-edit')
